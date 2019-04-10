a78fda6ba5282420c14f5d2a4e7312ebb605dbc1
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-04-10 21:39:11 +0300

    working inline mod and added gitignore

--- bot.js
+++ bot.js
@@ -29,50 +29,101 @@ bot.getMe().then(function(me) {
 bot.onText(/\/unisport(.*)/, function(msg, match) {
   var fromId = msg.chat.id; // get the id, of who is sending the message
   var kb = [
-    ["today"],
-    ["tomorrow", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"]
+    [
+      {
+        text: "today",
+        callback_data: "0"
+      }
+    ],
+    [
+      {
+        text: "tomorrow",
+        callback_data: "1"
+      },
+      {
+        text: "2",
+        callback_data: "2"
+      },
+      {
+        text: "3",
+        callback_data: "3"
+      }
+    ],
+    [
+      {
+        text: "4",
+        callback_data: "4"
+      },
+      {
+        text: "5",
+        callback_data: "5"
+      },
+      {
+        text: "6",
+        callback_data: "6"
+      }
+    ],
+    [
+      {
+        text: "7",
+        callback_data: "7"
+      },
+      {
+        text: "8",
+        callback_data: "8"
+      },
+      {
+        text: "9",
+        callback_data: "9"
+      }
+    ]
   ]; // The keyboard array
   bot.sendMessage(
     msg.chat.id,
     "Which schedule (how many days from today) do you want?",
     {
       reply_markup: {
-        keyboard: kb,
-        one_time_keyboard: true
+        inline_keyboard: kb
       }
     }
   );
+});
 
-  // allowing only numbers and letters
-  var reg = /^[a-zA-Z0-9_.-]*$/;
-  bot.onText(reg, function(msg, match) {
-    bot.sendMessage(msg.chat.id, "You selected " + match);
-    console.log(match);
-    var d = match[0] == "today" ? 0 : match[0] == "tomorrow" ? 1 : match[0];
-    var day = d ? moment().add(d, "days") : moment();
-    getUnisportData(day.format("YYYY-MM-DD"))
-      .then(m => {
-        bot.sendMessage(
-          fromId,
+// Handle callback queries
+bot.on("callback_query", function onCallbackQuery(callbackQuery) {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  var fromId = msg.chat.id;
+
+  var d = action == "today" ? 0 : action == "tomorrow" ? 1 : action;
+  var day = d ? moment().add(d, "days") : moment();
+
+  getUnisportData(day.format("YYYY-MM-DD"))
+    .then(m => {
+      bot.sendMessage(
+        fromId,
+        "*" +
+          day.format("dddd DD.MM.YYYY [@ Otahalli:]") +
           "*" +
-            day.format("dddd DD.MM.YYYY [@ Otahalli:]") +
-            "*" +
-            "\n> " +
-            m.join("\n> "),
-          { parse_mode: "Markdown" }
-        );
-      })
-      .catch(() => bot.sendMessage(fromId, "Couldn't fetch data"));
-    var selectedSerie = msg.query;
-  });
+          "\n> " +
+          m.join("\n> "),
+        { parse_mode: "Markdown" }
+      );
+    })
+    .then(() => {
+      bot.deleteMessage(fromId, msg.message_id);
+    })
+    .catch(err => {
+      console.log(err);
+      bot.sendMessage(fromId, "Couldn't fetch data");
+    });
+
+  bot.editMessageText(text, opts);
 });
 
 // match /help
 bot.onText(/\/help/, function(msg, match) {
-  var text =
-    "By sending /unisport you get the unisport classes of this day in Otahalli. By adding a space and a number to the end, you can get the classes of that day which is that many days away. For example '/unisport 2' gives the classes of the day after tomorrow.";
+  var text = "With /unisport you get the unisport classes in Otahalli. .";
   var fromId = msg.chat.id; // get the id, of who is sending the message
   bot.sendMessage(fromId, text);
 });
